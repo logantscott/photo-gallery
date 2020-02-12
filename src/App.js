72e8052b0076a368d8650f97bc28a261c5b53bd6
Logan Scott <logantscott@gmail.com>
2020-02-12 13:41:03 -0800

    third filter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { Keywords, Horns } from './Options.js';
 
 export default class App extends Component {
   state = { selected: null,
-            horns: null };
+            horns: null,
+            third: null };
 
   render() {
     let hornsOptions = images
@@ -20,16 +21,16 @@ export default class App extends Component {
         return hornsOptions.indexOf(horn) === i
       })
 
-      let keywordOptions = images
-        .map(image => {
-          return image.keyword
-        })
+    let keywordOptions = images
+      .map(image => {
+        return image.keyword
+      })
 
-      keywordOptions = keywordOptions
-        .filter((keyword, i) => {
-          return keywordOptions.indexOf(keyword) === i
-        })
-        .sort()
+    keywordOptions = keywordOptions
+      .filter((keyword, i) => {
+        return keywordOptions.indexOf(keyword) === i
+      })
+      .sort()
 
     const imageNodes = images
       .filter(image => {
@@ -41,6 +42,11 @@ export default class App extends Component {
         if (!this.state.horns) return true;
 
         return Number(this.state.horns) === image.horns
+      })
+      .filter(image => {
+        if (!this.state.third) return true;
+
+        return (Number(this.state.third)) !== image.horns
       });
 
     
@@ -53,11 +59,16 @@ export default class App extends Component {
       this.setState({ horns: e.target.value });
     };
 
+    const handleThirdChange = e => {
+      this.setState({ third: e.target.value });
+    };
+
     return (
       <div className="App">
         <Header />
         <section className="options">
 
+            <label>Keyword: </label>
             <select className="images-keyword-filter" onChange={handleChange}>
               <option value="" defaultValue>
                 All Keywords
@@ -66,7 +77,17 @@ export default class App extends Component {
                 return <Keywords keyword={keyword} key={i} />
               })}
             </select>
+            <label> Horns: </label>
             <select className="images-horns-filter" onChange={handleHornsChange}>
+              <option value="" defaultValue>
+                All Horns
+              </option>
+              {hornsOptions.map((horn, i) => (
+                <Horns horn={horn} key={i} />
+              ))}
+            </select>
+            <label> Not Horns: </label>
+            <select className="images-third-filter" onChange={handleThirdChange}>
               <option value="" defaultValue>
                 All Horns
               </option>
